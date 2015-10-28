9f125e2f7d8b446b6d12df5a2eed6f10fa468c80
ddizoya <ddizoya@atanasoft04.danielcastelao.org>
2015-10-28 11:48:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ddizoya/domotica2_0/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.ddizoya.domotica2_0;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        SeekBar sb = (SeekBar) findViewById(R.id.barra);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cambiarToast(View w){
+       Button b = (Button) findViewById(R.id.onoff);
+        if (b.toString().equals(getText(R.string.on))) {
+            b.setText(getText(R.string.off));
+            Toast.makeText(MainActivity.this, "Has cambiado la luz a " + getText(R.string.off), Toast.LENGTH_SHORT).show();
+        } else {
+            b.setText(getText(R.string.on));
+            Toast.makeText(MainActivity.this, "Has cambiado la luz a " + getText(R.string.on), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
+}
